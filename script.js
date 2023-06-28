0a592092440a5db230a71707cd60758b414bf155
Satish <satishkrgupta555@gmail.com>
2023-06-28 19:36:14 +0530

    a very basic calculator v0.1

---
+++ script.js
@@ -0,0 +1,14 @@
+function appendValue(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculate() {
+    const expression = document.getElementById('display').value;
+    const result = eval(expression);
+    document.getElementById('display').value = result;
+  }
+  
